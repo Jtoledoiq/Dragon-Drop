8c2d276a8fe29fd530741c9ce83b65dd8e014363
Jesús Toledo <43845286+Jtoledoiq@users.noreply.github.com>
2020-10-07 12:59:54 +0200

    DOM selection and Rendering

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,16 @@
 "use strict";
+var ProjectInput = /** @class */ (function () {
+    function ProjectInput() {
+        this.templateElement = document.getElementById('project-input');
+        this.hostElement = document.getElementById('app');
+        var importNode = document.importNode(this.templateElement.content, true);
+        this.element = importNode.firstElementChild;
+        this.attach();
+        console.log(importNode);
+    }
+    ProjectInput.prototype.attach = function () {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    };
+    return ProjectInput;
+}());
+var prjInput = new ProjectInput();
