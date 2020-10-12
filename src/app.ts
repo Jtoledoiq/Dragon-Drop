f5ba074ecc733782615718dd856af2eca10571de
Jesús Toledo <43845286+Jtoledoiq@users.noreply.github.com>
2020-10-12 12:37:01 +0200

    add filtering and fix bug

--- src/app.ts
+++ src/app.ts
@@ -101,7 +101,12 @@ class ProjectList {
         this.element.id =`${this.type}-projects`;
 
         projectState.addListener((projects: Project[])=>{
-            this.assignedProjects = projects
+            this.assignedProjects = projects.filter(prj => {
+                if(this.type === "active") {
+                    return prj.status === ProjectStatus.Active;
+                }
+                return prj.status === ProjectStatus.Finished;
+            });
             this.renderProjects();
         });
         this.attach();
@@ -110,6 +115,7 @@ class ProjectList {
 
     private renderProjects() {
         const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
+        listEl.innerHTML = '';
         for (const prjItem of this.assignedProjects) {
             const listItem = document.createElement('li');
             listItem.textContent = prjItem.title;
